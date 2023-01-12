c296561a31294fb4df2f76a643ae7a7c144aadff
punita <punitayadav406@gmail.com>
2023-01-12 18:25:53 +0400

    pushing all the todo react files to github

---
+++ src/reducers/taskSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import taskData from '../data/tasks.json';
+
+const initialState =[...taskData]
+
+
+
+ export const taskSlice = createSlice({
+
+    name:'tasks',
+    initialState,
+    reducers:{
+
+        addTasks:(action) => {
+            return action.payload
+        },
+        addTask:(state,action) => {
+            return [...state,action.payload]
+        },
+        deleteTask:(state,action) => {
+            return state.filter(task => task.id !==action.payload)
+        },
+        updateTask:(state,action) => {
+            return state.map(task => task.id === action.payload.id ? {...task,...action.payload}:task)
+        },
+    }
+})
+
+export const {addTask,updateTask,deleteTask,addTasks}=taskSlice.actions;
+
+export default taskSlice.reducer;
